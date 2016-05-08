ce28ce65f265f6380f851de312a7d7b932498f72
Марко М. Костић (Marko M. Kostić) <marko.m.kostic@gmail.com>
2016-05-08 19:59:58 +0200

    Fixed an issue where programdir variable ends in a zipfile when using a frozen program via cx_freeze, on Windows.

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
 
 programdir = os.path.dirname(os.path.realpath(__file__))
+# If we are frozen via cx_Freeze on Windows, we need to readjust the programdir var
+if programdir[-11:] == 'library.zip':
+    programdir = programdir[:-12]
